32976c4317719cd31fe1e7baa2d828bf4df9f90e
MarkHoward <markjameshoward@hotmail.co.uk>
2019-11-14 13:47:31 +1300

    Contentful first actual blog added!

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout.js"
-import Head from '../components/head'
+import Head from "../components/head"
 
 const About = () => {
   return (
     <Layout>
-      <Head title="About"/>
+      <Head title="About" />
       <h1>About</h1>
       <p>This is the about page</p>
       <Link to="/contact">Contact</Link>

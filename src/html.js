bd23ea5a5a367ad1b2033516e2ce4209b4cf1a47
MarkHoward <markjameshoward@hotmail.co.uk>
2019-11-23 21:15:28 +1300

    plugin analytics attempt

--- src/html.js
+++ src/html.js
@@ -5,16 +5,6 @@ export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
       <head>
-        
-      <!-- Global site tag (gtag.js) - Google Analytics -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-153067406-1"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-153067406-1');
-      </script>
 
       <script data-ad-client="ca-pub-6571414832573314" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         <meta charSet="utf-8" />

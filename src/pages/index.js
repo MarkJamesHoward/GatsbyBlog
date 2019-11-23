8705295fb9ea739121894970a6ab1825948b2b14
MarkHoward <markjameshoward@hotmail.co.uk>
2019-11-23 14:44:42 +1300

    added links to all my websites

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,23 @@ const IndexPage = () => {
       <Head title="Home"/>
       <h1>Hello</h1>
       <h2> I'm mark a full stack developer in Wellington</h2>
+      <ul>
+        <li>
+          <h2>
+            <Link to="/Blog">Blog</Link>
+          </h2>
+        </li>
+        <li>
+          <h2>
+            <a href="https://littleheroes.online" >LittleHeroes</a>
+          </h2>
+        </li>
+        <li>
+          <h2>
+            <a href="https://creditcardwizard.co.nz">CreditCardWizard</a>
+          </h2>
+        </li>
+      </ul>
     </Layout>
   )
 }

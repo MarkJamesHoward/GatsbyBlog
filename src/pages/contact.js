2c82121135929f6f691ceac13388d27ef4f76813
MarkHoward <markjameshoward@hotmail.co.uk>
2019-12-13 14:00:19 +1300

    line height now 2

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,17 +1,18 @@
 import React from "react"
 import Layout from "../components/layout.js"
-import Head from '../components/head'
-
+import Head from "../components/head"
 
 const Contact = () => {
   return (
     <Layout>
-      <Head title="Contact"/>
+      <Head title="Contact" />
       <h1>Contact</h1>
-      <p>This is the contact page</p>
-      <a target="_" href="https://twitter.com/bigusbirdicus">
-        Twitter
-      </a>
+      <p>
+        Want to contact me? Send me a DM on &nbsp;
+        <a target="_" href="https://twitter.com/bigusbirdicus">
+          Twitter
+        </a>
+      </p>
     </Layout>
   )
 }
